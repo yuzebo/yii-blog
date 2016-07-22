286e28a4b26800680354cff2e39fb7554cf39439
yuzebo <yuzebo199233@126.com>
2016-07-22 11:29:17 +0800

    creating models and implementing CRUD operations by gii

--- blog/protected/config/database.php
+++ blog/protected/config/database.php
@@ -2,13 +2,13 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+
+	'connectionString' => 'mysql:host=localhost;dbname=blog',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '123456',
 	'charset' => 'utf8',
-	*/
+	'tablePrefix' => 'tbl_'
 );

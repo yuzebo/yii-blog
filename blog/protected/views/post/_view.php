cdf3d68f1748b86db19e2cfcead1e6bcd395959b
yuzebo <yuzebo199233@126.com>
2016-08-02 15:15:12 +0800

    Setting Post and Comments about css operations,Modify some bugs

--- blog/protected/views/post/_view.php
+++ blog/protected/views/post/_view.php
@@ -3,41 +3,27 @@
 /* @var $data Post */
 ?>
 
-<div class="view">
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php echo CHtml::encode($data->title); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
-	<?php echo CHtml::encode($data->content); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('tags')); ?>:</b>
-	<?php echo CHtml::encode($data->tags); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
-	<?php echo CHtml::encode($data->status); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('create_time')); ?>:</b>
-	<?php echo CHtml::encode($data->create_time); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('update_time')); ?>:</b>
-	<?php echo CHtml::encode($data->update_time); ?>
-	<br />
-
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('author_id')); ?>:</b>
-	<?php echo CHtml::encode($data->author_id); ?>
-	<br />
-
-	*/ ?>
-
+<div class="post">
+	<div class="title">
+		<?php echo CHtml::link(CHtml::encode($data->title),$data->url); ?>
+	</div>
+	<div class="author">
+		posted by<?php echo $data->author->username . ' on ' . date('F j, Y',$data->create_time); ?>
+	</div>
+	<div class="content">
+		<?php
+			$this->beginWidget('CMarkdown',array('purifyOutput'=>true));
+			echo $data->content;
+			$this->endWidget();
+		?>
+<!--		--><?php //$this->widget('ext.WSocialButton.WSocialButton', array('style'=>'standard'));?>
+	</div>
+	<div class="nav">
+		<b>Tags:</b>
+		<?php echo implode(', ',$data->tagLinks); ?>
+		</br>
+		<?php echo CHtml::link('Permalink', $data->url); ?>|
+		<?php echo CHtml::link("Comments({$data->commentCount})",$data->url.'#comments'); ?>|
+		Last updated on <?php echo date('F j, Y',$data->update_time); ?>
+	</div>
 </div>
--- blog/protected/views/post/index.php
+++ blog/protected/views/post/index.php
@@ -1,17 +1,3 @@
-<?php
-/* @var $this PostController */
-/* @var $dataProvider CActiveDataProvider */
-
-$this->breadcrumbs=array(
-	'Posts',
-);
-
-$this->menu=array(
-	array('label'=>'Create Post', 'url'=>array('create')),
-	array('label'=>'Manage Post', 'url'=>array('admin')),
-);
-?>
-
 <?php if(!empty($_GET['tag'])): ?>
 	<h1>Posts Tagged with <i><?php echo CHtml::encode($_GET['tag']); ?></i></h1>
 <?php endif; ?>
